c7d0fcd149404e591d63da9cc899abadf1cb370e
mrk-hnr <mrk.hnr@hotmail.com>
2024-11-19 12:34:56 +0800

    Implemented edit page styling

--- update.php
+++ update.php
@@ -7,14 +7,18 @@ include('includes/db.php');
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles/style.css?v=<?php echo time(); ?>">
 
     <title>Edit Profile</title>
 </head>
-<body>
+<body class="edit-body">
     
     <div class="container">
-      <h3>Edit Profile</h3>
-      <a href="./index.php">BACK</a>
+
+      <div class="edit-nav">
+        <h3>Edit Profile</h3>
+        <a href="./index.php">BACK</a>
+      </div>
       
       <?php
       if(isset($_GET['id'])) {
@@ -32,25 +36,46 @@ include('includes/db.php');
 
       <form action="includes/update-user.php" method="POST">
         <input type="hidden" name="user_id" value="<?php echo $result->id?>" />
-        <div>
-          <label>Username</label>
-          <input type="text" name="username" value="<?= $result->username; ?>" required />
-        </div>
         
+        <div class="input-group">
+          <input class="input" required autocomplete="off" type="text" name="username" id="username" >
+          <label class="label" for="username">Username</label>
+        </div>
+
+        <div class="input-group">
+          <input class="input" required autocomplete="off" type="text" name="email" id="email" >
+          <label class="label" for="email">Email</label>
+        </div>
+
+        <div class="input-group">
+          <input class="input" required autocomplete="off" type="password" name="password" id="password" >
+          <label class="label" for="password">Password</label>
+        </div>
+
+        <div class="input-group">
+          <input class="input" required autocomplete="off" type="password" name="password" id="confirm_password" >
+          <label class="label" for="password">Retype Password</label>
+        </div>
+
+
+        <!-- <div>
+          <input type="text" placeholder="<?= $result->username; ?>" required />
+        </div> 
+
         <div>
           <label>Email</label>
           <input type="text" name="email" value="<?= $result->email; ?>" />
-        </div>
+        </div> 
         
         <div>
           <label>Password</label>
           <input type="text" name="password" placeholder="<?= $result->pass; ?>" required />
-        </div>
+        </div> 
         
         <div>
           <label>Retype Password</label>
           <input type="text" name="confirm_password" placeholder="<?= $result->pass; ?>" required />
-        </div>
+        </div> -->
         
         <div>
           <button type="submit" name="update_user" class="btn btn-primary">Save Changes</button>
@@ -59,5 +84,10 @@ include('includes/db.php');
       </form>
 
     </div>
+
+
+
+
+
   </body>
 </html>
